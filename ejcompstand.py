fc50094619b4361be6bf3f0b1028cc07df098ce3
Vassiliy V Alferov <ya-ikmik2012@yandex.ru>
2015-07-12 13:13:00 +0000

    Pretified

--- ejcompstand.py
+++ ejcompstand.py
@@ -10,26 +10,26 @@ import pymysql
 class Contest:
     def __init__(self, d):
         self.name = d["contest_name"]
+        self.prob_num = d["prob_num"]
         self.contest_id = d["contest_id"]
         self.scoring_type = d["contest_scoring_type"]
         
-    def GetRow(self, uid, mysql_con):
+    def GetRow(self, uid, mysql_con, cell_num):
         cursor = mysql_con.cursor()
         runs = cursor.execute("select prob_id, score, status from runs where user_id=" + str(user_id) + " and contest_id=" + str(self.contest_id) + ";")
         if self.scoring_type == "acm":
             s = "<tr><td>"
             s += self.name + "</td>"
-            prob_num = 0
+            prob_num = self.prob_num
             runs = []
             for prob_id, score, status in cursor:
                 runs.append((prob_id, score, status))
-                prob_num = max(prob_num, prob_id)
             ok = [False] * prob_num
             failed = [0] * prob_num
             for prob_id, score, status in runs:
                 if status == 0:
                     ok[prob_id - 1] = True
-                else:
+                elif not ok[prob_id - 1]:
                     failed[prob_id - 1] += 1
             for i in range(prob_num):
                 attr = ""
@@ -38,30 +38,42 @@ class Contest:
                     attr = " class=\"ok\""
                 elif failed[i]:
                     t = "-"
+                    attr = " class=\"fail\""
                 else:
                     t = "."
                 if failed[i]:
                     t += str(failed[i])
                 s += "<td" + attr + ">" + t + "</td>"
+            if prob_num < cell_num:
+                s += "<td colspan=" + str(cell_num - prob_num) + " class=\"disabled\"></td>"
+            s += "<td class=\"sum\">" + str(sum(map(int, ok))) + "</td>"
+            s += "<td>" + str(sum(failed)) + "</td>"
             s += "</tr>"
         elif self.scoring_type == "olympiad":
             s = "<tr><td>" + self.name + "</td>"
-            prob_num = 0
+            prob_num = self.prob_num
             runs = []
             for prob_id, score, status in cursor:
                 runs.append((prob_id, score, status))
-                prob_num = max(prob_num, prob_id)
             res = [0] * prob_num
             tried = [False] * prob_num
+            ok = [False] * prob_num
             for prob_id, score, status in runs:
                 res[prob_id - 1] = max(res[prob_id - 1], score)
                 if score >= 0:
                     tried[prob_id - 1] = True
+                if status == 0:
+                    ok[prob_id - 1] = True
             for i in range(prob_num):
                 attr = ""
-                if res[i] == 100:
+                if ok[i]:
                     attr = " class=\"ok\""
+                elif tried[i]:
+                    attr = " class=\"fail\""
                 s += "<td" + attr + ">" + str(res[i]) + "</td>"
+            if prob_num < cell_num:
+                s += "<td colspan=" + str(cell_num - prob_num) + " class=\"disabled\"></td>"
+            s += "<td class=\"sum\">" + str(sum(res)) + "</td>"
             s += "</tr>"
         cursor.close()
         return s
@@ -70,19 +82,37 @@ class Contest:
 class ContestGroup:
     def __init__(self, d):
         self.name = d["name"]
+        self.numeration = d["prob_numeration"]
         self.contests = []
+        self.prob_num = 0
+        self.scoring_type = "None"
         for cntst in d["contests"]:
             self.contests.append(Contest(cntst))
+        for cntst in self.contests:
+            self.prob_num = max(self.prob_num, cntst.prob_num)
+            assert self.scoring_type == "None" or self.scoring_type == cntst.scoring_type
+            self.scoring_type = cntst.scoring_type
 
     def GetTable(self, uid, mysql_con):
         rows = []
         for cntst in self.contests:
-            s = cntst.GetRow(uid, mysql_con)
+            s = cntst.GetRow(uid, mysql_con, self.prob_num)
             if len(s):
                 rows.append(s)
         if len(rows):
             ans = "<h1>" + self.name + "</h1>"
             ans += "<table border=1>"
+            ans += "<tr><td>P</td>"
+            if self.numeration == "latin":
+                first = ord("A")
+            else:
+                first = ord("1")
+            for i in range(self.prob_num):
+                ans += "<td>" + chr(first + i) + "</td>"
+            ans += "<td class=\"sum\">" + chr(0x03a3) + "</td>"
+            if self.scoring_type == "acm":
+                ans += "<td>S</td>"
+            ans += "</tr>"
             ans += "".join(rows)
             ans += "</table>"
         else:
