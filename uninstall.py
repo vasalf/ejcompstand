853d3965c58ad5d89eff5c93a5efbd3ab9c14aa2
Vassiliy V Alferov <ya-ikmik2012@yandex.ru>
2015-07-20 20:09:56 +0000

    bugfix

--- uninstall.py
+++ uninstall.py
@@ -5,7 +5,6 @@ import os
 
 os.system("rm -f " + prefix + "/bin/ejcompstand")
 os.system("rm -Rf " + prefix + "/share/ejcompsand")
-os.system("rm -Rf " + var_dir)
 os.system("rm -Rf " + http_htdocs_dir + "/ejcompstand")
 os.system("rm -f " + http_cgi_dir + "/ejcompstand")
 
